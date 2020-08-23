dd5c680c4a8eb28ff266bb80e6b9f17d9de3926d
Tapani Moilanen <moilanen.tapani@gmail.com>
2020-08-23 15:55:40 +0300

    feat: simple debug camera rotation controls

--- debug_controls/camera_target.gd
+++ debug_controls/camera_target.gd
@@ -8,22 +8,38 @@ extends Spatial
 #   key press time while InterpolatedCamera.speed controls smoothing
 
 export var speed: = 0.8
+export var rotation_speed: = 2
+
+var ID_PLANE = Vector3(1, 0, 1)
+# TODO: Z_MULTI should probably be replaced by scaling with rotation of camera..
+var Z_MULTI = 1.5
 
 func _physics_process(delta):
 	if get_parent().enabled:
 		var absolute_movement = speed * delta
+		# Movement horizontally is relative to rotation on plane
+		if Input.is_action_pressed('debug_camera_right'):
+			transform.origin += transform.basis.x * ID_PLANE * absolute_movement
+		if Input.is_action_pressed('debug_camera_left'):
+			transform.origin -= transform.basis.x * ID_PLANE * absolute_movement
 		# Due to Godot Y-is-down convention, Z axis movement is "inverted"
 		if Input.is_action_pressed('debug_camera_forward'):
-			transform.origin.z -= absolute_movement
-		if Input.is_action_pressed('debug_camera_back'):
-			transform.origin.z += absolute_movement
+			transform.origin -= \
+				transform.basis.z * ID_PLANE * absolute_movement * Z_MULTI
+		if Input.is_action_pressed('debug_camera_backward'):
+			transform.origin += \
+				transform.basis.z * ID_PLANE * absolute_movement * Z_MULTI
 		
-		if Input.is_action_pressed('debug_camera_right'):
-			transform.origin.x += absolute_movement
-		if Input.is_action_pressed('debug_camera_left'):
-			transform.origin.x -= absolute_movement
+		# Movement down/up is absolute
 		if Input.is_action_pressed('debug_camera_up'):
 			transform.origin.y += absolute_movement
 		if Input.is_action_pressed('debug_camera_down'):
 			transform.origin.y -= absolute_movement
+		
+		# Rotations relative to object but around Y axis only
+		var absolute_rotation = rotation_speed * delta
+		if Input.is_action_pressed('debug_camera_rotate_right'):
+			rotate(Vector3(0, -1, 0), absolute_rotation)
+		if Input.is_action_pressed('debug_camera_rotate_left'):
+			rotate(Vector3(0, 1, 0), absolute_rotation)
 
