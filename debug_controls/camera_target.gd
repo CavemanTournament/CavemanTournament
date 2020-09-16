c429b4c7d31b39bedecd9523329838fb44313f28
Tapani Moilanen <moilanen.tapani@gmail.com>
2020-09-16 17:16:19 +0300

    fix: debug camera uses normalized vectors for horizontal movement

--- debug_controls/camera_target.gd
+++ debug_controls/camera_target.gd
@@ -1,34 +1,40 @@
 extends Spatial
 
 # Debug camera movement
-# + Camera movement tied to physics delta with interpolated camera so
+# - Camera movement tied to physics delta with interpolated camera so
 #   that the amount of movement is relative to absolute key press time
 #   instead of fps press time but interpolation is smoothed with fps
-# + CameraTarget.speed controls how far the camera travels relative to
+# - CameraTarget.speed controls how far the camera travels relative to
 #   key press time while InterpolatedCamera.speed controls smoothing
 
 export var speed: = 0.8
 export var rotation_speed: = 2
 
 var ID_PLANE = Vector3(1, 0, 1)
-# TODO: Z_MULTI should probably be replaced by scaling with rotation of camera..
-var Z_MULTI = 1.5
 
 func _physics_process(delta):
 	if get_parent().enabled:
 		var absolute_movement = speed * delta
-		# Movement horizontally is relative to rotation on plane
+		
+		# Movement horizontally (x/z-axis) is relative to rotation on plane
+		var basis_x = transform.basis.x.normalized()
+		var basis_z = transform.basis.z.normalized()
+		
+		if basis_x == Vector3.ZERO:
+			basis_x = Vector3.AXIS_X
+		# Godot uses Y-is-down convention, z-axis movement is "inverted"
+		# Negative z-axis is away from you, i.e. forward
+		if basis_z == Vector3.ZERO:
+			basis_z = -Vector3.AXIS_Z 
+
 		if Input.is_action_pressed('debug_camera_right'):
-			transform.origin += transform.basis.x * ID_PLANE * absolute_movement
+			transform.origin += basis_x * ID_PLANE * absolute_movement
 		if Input.is_action_pressed('debug_camera_left'):
-			transform.origin -= transform.basis.x * ID_PLANE * absolute_movement
-		# Due to Godot Y-is-down convention, Z axis movement is "inverted"
+			transform.origin -= basis_x * ID_PLANE * absolute_movement
 		if Input.is_action_pressed('debug_camera_forward'):
-			transform.origin -= \
-				transform.basis.z * ID_PLANE * absolute_movement * Z_MULTI
+			transform.origin -= basis_z * ID_PLANE * absolute_movement
 		if Input.is_action_pressed('debug_camera_backward'):
-			transform.origin += \
-				transform.basis.z * ID_PLANE * absolute_movement * Z_MULTI
+			transform.origin += basis_z * ID_PLANE * absolute_movement
 		
 		# Movement down/up is absolute
 		if Input.is_action_pressed('debug_camera_up'):
